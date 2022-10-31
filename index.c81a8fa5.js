e6a452aff582bc1506e36db2d99ebc1b3da7879b
teiatan <protas.tb@gmail.com>
2022-10-31 03:40:59 +0000

    Deploying to gh-pages from @ teiatan/FoodSta-team5@cef2d9df790b19affe580c5afcd03174477721f7 🚀

---
+++ index.c81a8fa5.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:1e3,autoplay:!0,autoplaySpeed:2e3,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.c81a8fa5.js.map
