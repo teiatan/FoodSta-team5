413863f29a7f0ebcc5439a5024215c4b5714a87a
teiatan <protas.tb@gmail.com>
2022-11-02 07:40:38 +0000

    Deploying to gh-pages from @ teiatan/FoodSta-team5@380909ffe9663b306a2d2ac812eacff663cac240 🚀

---
+++ index.a2777ee5.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector(".open-menu-btn"),closeModalBtn:document.querySelector(".menu-close-btn"),menuModal:document.querySelector(".mob-menu")};function n(){e.menuModal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",n),e.closeModalBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.a2777ee5.js.map
---
+++ index.bc4bdd26.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector(".open-menu-btn"),closeModalBtn:document.querySelector(".menu-close-btn"),menuModal:document.querySelector(".mob-menu")};function n(){e.menuModal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",n),e.closeModalBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.bc4bdd26.js.map
