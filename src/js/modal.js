8fcb52ab1cd5a46b9032e4754243a03ee81b74dd
teiatan <92535482+teiatan@users.noreply.github.com>
2022-11-04 11:12:20 +0200

    universal button (opening)
    all order-buttons have same attribute for opening modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn: document.querySelectorAll("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  //refs.openModalBtn.map(btn => btn.addEventListener("click", toggleModal));
+  refs.openModalBtn.forEach(function (btn) {btn.addEventListener("click", toggleModal)})
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  //console.log(refs.openModalBtn);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
