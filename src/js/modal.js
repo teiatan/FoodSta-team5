0f805999667a562a1f65d890bdcce8e9022374a9
teiatan <92535482+teiatan@users.noreply.github.com>
2022-11-04 11:52:38 +0200

    universal order-button (everything)
    add order-button.js for adding modifications of btn for different section
    
    all button are added by using include

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,12 +5,13 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  //refs.openModalBtn.map(btn => btn.addEventListener("click", toggleModal));
+
   refs.openModalBtn.forEach(function (btn) {btn.addEventListener("click", toggleModal)})
   refs.closeModalBtn.addEventListener("click", toggleModal);
-  //console.log(refs.openModalBtn);
+
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
---
+++ src/js/order-button.js
@@ -0,0 +1,4 @@
+const offeringsBtn = document.querySelector(".offerings [data-modal-open]");
+//offeringsBtn.classList.add(`order-button--white`);
+offeringsBtn.classList.replace("order-button", "order-button--white");
+console.log(offeringsBtn);
