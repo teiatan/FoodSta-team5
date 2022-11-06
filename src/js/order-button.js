174671b730cd9b281c104bee0f6e53b4afdcc72a
teiatan <92535482+teiatan@users.noreply.github.com>
2022-11-06 13:06:51 +0200

    go to top button

--- src/js/order-button.js
+++ src/js/order-button.js
@@ -1,4 +1,22 @@
 const offeringsBtn = document.querySelector(".offerings [data-modal-open]");
 //offeringsBtn.classList.add(`order-button--white`);
 offeringsBtn.classList.replace("order-button", "order-button--white");
-console.log(offeringsBtn);
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
